9846939ae623e7461855691e643f522069370ffa
Zach Morris <zach-morris@users.noreply.github.com>
2020-04-07 16:49:40 -0700

    Update test
    Fixes crash with a slight pause

--- lib/archive_tool/run_test.py
+++ lib/archive_tool/run_test.py
@@ -145,11 +145,11 @@ if not WIN.getProperty('archive_tool.script_started'):
 	except Exception as exc:
 		xbmc.log(msg='archive_tool:  Error in test_iso.  Exception %(exc)s' % {'exc': exc}, level=xbmc.LOGERROR)
 
- 	#rar (non solid) Test
+ 	# rar (non solid) Test with libarchive
 	try:
 		archive_file =  os.path.join(xbmc.translatePath(addon_handle.getAddonInfo('path')),'resources','test.rar')
 		test_folder = os.path.join(xbmc.translatePath(addon_handle.getAddonInfo('path')),'resources','test_rar','')
-		xbmc.log(msg='archive_tool:  Testing %(archive_file)s' % {'archive_file':archive_file}, level=xbmc.LOGDEBUG)
+		xbmc.log(msg='archive_tool:  Testing with vfs.libarchive %(archive_file)s' % {'archive_file':archive_file}, level=xbmc.LOGDEBUG)
 		my_archive = archive_tool.archive_tool(archive_file = archive_file,directory_out = test_folder) #Current archive object using vfs.libarchive
 		file_listing = my_archive.list_all() #Lists all files in the archive
 		xbmc.log(msg='archive_tool:  File listing:\n%(file_listing)s' % {'file_listing':'\n'.join(file_listing)}, level=xbmc.LOGDEBUG)
@@ -162,30 +162,54 @@ if not WIN.getProperty('archive_tool.script_started'):
 			if xbmcvfs.rmdir(test_folder,True):
 				xbmc.log(msg='archive_tool:  test_rar folder deleted', level=xbmc.LOGDEBUG)
 	except Exception as exc:
-		xbmc.log(msg='archive_tool:  Error in test_rar.  Exception %(exc)s' % {'exc': exc}, level=xbmc.LOGERROR)
+		xbmc.log(msg='archive_tool:  Error in test_rar using vfs.libarchive.  Exception %(exc)s' % {'exc': exc}, level=xbmc.LOGERROR)
 
- 	#rar (solid) Test, currently crashes Kodi
+	#rar (non solid) Test with rar
+	xbmc.sleep(1000)
+	xbmc.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Addons.SetAddonEnabled","params":{"addonid": "vfs.libarchive", "enabled": false} }') #Temporarily disable vfs.libarchive
+	xbmc.sleep(1000)
+	try:
+		archive_file =  os.path.join(xbmc.translatePath(addon_handle.getAddonInfo('path')),'resources','test.rar')
+		test_folder = os.path.join(xbmc.translatePath(addon_handle.getAddonInfo('path')),'resources','test_rar','')
+		xbmc.log(msg='archive_tool:  Testing with vfs.rar %(archive_file)s' % {'archive_file':archive_file}, level=xbmc.LOGDEBUG)
+		my_archive = archive_tool.archive_tool(archive_file = archive_file,directory_out = test_folder, use_vfs_rar=True) #Current archive object using vfs.libarchive
+		file_listing = my_archive.list_all() #Lists all files in the archive
+		xbmc.log(msg='archive_tool:  File listing:\n%(file_listing)s' % {'file_listing':'\n'.join(file_listing)}, level=xbmc.LOGDEBUG)
+		stat_listing = my_archive.stat_all() #Dict of all files in the archive containing fullpath, filename, file size (extracted)
+		xbmc.log(msg='archive_tool:  File Stats:\n%(stat_listing)s' % {'stat_listing':'\n'.join(['Fullpath: %(file_fp)s, Filename: %(file_fn)s, Size: %(file_s)s,'%{'file_fp':x['fullpath'],'file_fn':x['filename'],'file_s':x['size']} for x in stat_listing])}, level=xbmc.LOGDEBUG)
+		xbmc.log(msg='archive_tool:  File Extraction Starting', level=xbmc.LOGDEBUG)
+		files_extracted, success_of_extraction = my_archive.extract()  #Extracts all files to directory_out, returns list of files extracted and True/False for extraction success.  Defaults to extract all files in the archive.
+		xbmc.log(msg='archive_tool:  Extraction success returned %(ex_success)s for test_rar.  Files:\n%(file_listing)s' % {'ex_success':success_of_extraction,'file_listing':'\n'.join(files_extracted)}, level=xbmc.LOGDEBUG)
+		if success_of_extraction and delete_files_after_extraction:
+			if xbmcvfs.rmdir(test_folder,True):
+				xbmc.log(msg='archive_tool:  test_rar folder deleted', level=xbmc.LOGDEBUG)
+	except Exception as exc:
+		xbmc.log(msg='archive_tool:  Error in test_rar using vfs.rar.  Exception %(exc)s' % {'exc': exc}, level=xbmc.LOGERROR)
+	# xbmc.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Addons.SetAddonEnabled","params":{"addonid": "vfs.libarchive", "enabled": true} }') #Temporarily disable vfs.libarchive
+
+
+ 	# rar (solid) Test
 	# xbmc.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Addons.SetAddonEnabled","params":{"addonid": "vfs.libarchive", "enabled": false} }') #Temporarily disable vfs.libarchive
 	# xbmc.sleep(1)
-	# try:
-	# 	archive_file =  os.path.join(xbmc.translatePath(addon_handle.getAddonInfo('path')),'resources','example.rar')
-	# 	test_folder = os.path.join(xbmc.translatePath(addon_handle.getAddonInfo('path')),'resources','example_rar','')
-	# 	xbmc.log(msg='archive_tool:  Testing %(archive_file)s' % {'archive_file':archive_file}, level=xbmc.LOGDEBUG)
-	# 	my_archive = archive_tool.archive_tool(archive_file = archive_file,directory_out = test_folder, use_vfs_rar=True) #Current archive object using vfs.libarchive
-	# 	file_listing = my_archive.list_all() #Lists all files in the archive
-	# 	xbmc.log(msg='archive_tool:  File listing:\n%(file_listing)s' % {'file_listing':'\n'.join(file_listing)}, level=xbmc.LOGDEBUG)
-	# 	stat_listing = my_archive.stat_all() #Dict of all files in the archive containing fullpath, filename, file size (extracted)
-	# 	xbmc.log(msg='archive_tool:  File Stats:\n%(stat_listing)s' % {'stat_listing':'\n'.join(['Fullpath: %(file_fp)s, Filename: %(file_fn)s, Size: %(file_s)s,'%{'file_fp':x['fullpath'],'file_fn':x['filename'],'file_s':x['size']} for x in stat_listing])}, level=xbmc.LOGDEBUG)
-	# 	xbmc.log(msg='archive_tool:  File Extraction Starting', level=xbmc.LOGDEBUG)
-	# 	files_extracted, success_of_extraction = my_archive.extract()  #Extracts all files to directory_out, returns list of files extracted and True/False for extraction success.  Defaults to extract all files in the archive.
-	# 	xbmc.log(msg='archive_tool:  Extraction success returned %(ex_success)s for example_rar.  Files:\n%(file_listing)s' % {'ex_success':success_of_extraction,'file_listing':'\n'.join(files_extracted)}, level=xbmc.LOGDEBUG)
-	# 	if success_of_extraction and delete_files_after_extraction:
-	# 		if xbmcvfs.rmdir(test_folder,True):
-	# 			xbmc.log(msg='archive_tool:  example_rar folder deleted', level=xbmc.LOGDEBUG)
-	# except Exception as exc:
-	# 	xbmc.log(msg='archive_tool:  Error in example_rar.  Exception %(exc)s' % {'exc': exc}, level=xbmc.LOGERROR)
-	# xbmc.sleep(1)
-	# xbmc.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Addons.SetAddonEnabled","params":{"addonid": "vfs.libarchive", "enabled": true} }') #Temporarily disable vfs.libarchive
+	try:
+		archive_file =  os.path.join(xbmc.translatePath(addon_handle.getAddonInfo('path')),'resources','example.rar')
+		test_folder = os.path.join(xbmc.translatePath(addon_handle.getAddonInfo('path')),'resources','example_rar','')
+		xbmc.log(msg='archive_tool:  Testing %(archive_file)s' % {'archive_file':archive_file}, level=xbmc.LOGDEBUG)
+		my_archive = archive_tool.archive_tool(archive_file = archive_file,directory_out = test_folder, use_vfs_rar=True) #Current archive object using vfs.libarchive
+		file_listing = my_archive.list_all() #Lists all files in the archive
+		xbmc.log(msg='archive_tool:  File listing:\n%(file_listing)s' % {'file_listing':'\n'.join(file_listing)}, level=xbmc.LOGDEBUG)
+		stat_listing = my_archive.stat_all() #Dict of all files in the archive containing fullpath, filename, file size (extracted)
+		xbmc.log(msg='archive_tool:  File Stats:\n%(stat_listing)s' % {'stat_listing':'\n'.join(['Fullpath: %(file_fp)s, Filename: %(file_fn)s, Size: %(file_s)s,'%{'file_fp':x['fullpath'],'file_fn':x['filename'],'file_s':x['size']} for x in stat_listing])}, level=xbmc.LOGDEBUG)
+		xbmc.log(msg='archive_tool:  File Extraction Starting', level=xbmc.LOGDEBUG)
+		files_extracted, success_of_extraction = my_archive.extract()  #Extracts all files to directory_out, returns list of files extracted and True/False for extraction success.  Defaults to extract all files in the archive.
+		xbmc.log(msg='archive_tool:  Extraction success returned %(ex_success)s for example_rar.  Files:\n%(file_listing)s' % {'ex_success':success_of_extraction,'file_listing':'\n'.join(files_extracted)}, level=xbmc.LOGDEBUG)
+		if success_of_extraction and delete_files_after_extraction:
+			if xbmcvfs.rmdir(test_folder,True):
+				xbmc.log(msg='archive_tool:  example_rar folder deleted', level=xbmc.LOGDEBUG)
+	except Exception as exc:
+		xbmc.log(msg='archive_tool:  Error in example_rar.  Exception %(exc)s' % {'exc': exc}, level=xbmc.LOGERROR)
+	xbmc.sleep(1000)
+	xbmc.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Addons.SetAddonEnabled","params":{"addonid": "vfs.libarchive", "enabled": true} }') #Temporarily disable vfs.libarchive
 
 
 	ok_ret = current_dialog.ok('Test Complete','Test Completed.  Check your debug log for results.')
